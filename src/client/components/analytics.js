09a59b152d40a211013c51fa96d2facb503ed652
Geoff Ren <geoff.ren@vanderbilt.edu>
2018-04-04 23:52:44 -0500

    Override

---
+++ src/client/components/analytics.js
@@ -0,0 +1,80 @@
+'use strict';
+
+
+import React, { Component }     from 'react';
+import { withRouter }           from 'react-router';
+const Influx = require('influx');
+const chart = require('chart.js');
+
+class Analytics extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            test: "JUST IN PLACE FOR NOW"
+        };
+        this.influx = new Influx.InfluxDB({
+            host: 'localhost',
+            database: 'sterling_ranch',
+            schema: [
+                {
+                    measurement: 'value_refresh',
+                    tags: [
+                        'id_on_network',
+                        'home_id',
+                        'node_id',
+                        'value_id',
+                        'manufacturer_id',
+                        'product_id',
+                        'label'
+                    ],
+                    fields: {
+                        data: Influx.FieldType.FLOAT,
+                        units: Influx.FieldType.STRING,
+                        type: Influx.FieldType.STRING,
+                        type_val: Influx.FieldType.INTEGER
+                    }
+                }
+            ]
+        });
+    }
+
+    componentDidMount() {
+        this.influx.query(
+            `select * from value_refresh where "label" = 'Relative Humidity'`
+        ).then(results => {
+            let xAxis = [];
+            let data = [];
+            console.log('Processing');
+            results.forEach(result => {
+                xAxis.push(result.time);
+                data.push(result.data);
+                console.log(result);
+            });
+            // console.log(xAxis + data);
+            console.log('Done');
+            let ctx = document.getElementById("myChart").getContext('2d');
+            let myChart = new Chart(ctx, {
+              type: 'line',
+              data: {
+                labels: xAxis,
+                datasets: [{
+                  label: 'Relative Humidity',
+                  data: data,
+                  backgroundColor: "rgba(153,255,51,0.4)"
+                }]
+              }
+            });
+        }).catch(err => {
+            return 'ERROR';
+        });
+    }
+
+    render() {
+        return <div>
+            <p>This is a chart</p>
+            <canvas id="myChart" width="400" height="400" style={{width: 800, height: 450}}></canvas>
+        </div>
+    };
+}
+
+export default withRouter(Analytics);
